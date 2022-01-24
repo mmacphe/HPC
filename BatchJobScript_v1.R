345d0961573a5b51e87f0fae6774a67ac510aa1d
Maggie MacPherson <maggie.macpherson@gmail.com>
2022-01-24 15:15:02 -0600

    Update BatchJobScript_v1.R

--- BatchJobScript_v1.R
+++ BatchJobScript_v1.R
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn10      #Number of nodes and processors per node
+#PBS -l nodes=1:ppn20      #Number of nodes and processors per node
 #PBS -l walltime=48:00:00  #Maximum wall time
 #PBS -N Tyrannidae_subsp   #Job name; default is name of script
 #PBS -o <file name>        #File name for standard output; default system will name
 #PBS -e <file name>        #File name for standard error; default system will name
-#PBS -q single             #Queue name for serial job; most common is checkpt or workq
+#PBS -q checkpt            #Queue name for serial job; most common is checkpt or workq
 #PBS -A hpc_masonlab03     #Allocation name
 #PBS -m e                  #Send mail when job ends
 #PBS -M maggie.macpherson@gmail.com #Send mail to this address
