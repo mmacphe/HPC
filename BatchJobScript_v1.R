440c43b5bdf8cde2b01d5a9cd64ac5545722fede
Maggie MacPherson <maggie.macpherson@gmail.com>
2022-01-24 14:41:34 -0600

    Create BatchJobScript_v1.R

---
+++ BatchJobScript_v1.R
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn20      #Number of nodes and processors per node
+#PBS -l walltime=48:00:00  #Maximum wall time
+#PBS -N Tyrannidae_subsp   #Job name; default is name of script
+#PBS -o <file name>        #File name for standard output; default system will name
+#PBS -e <file name>        #File name for standard error; default system will name
+#PBS -q single             #Queue name for serial job; most common is checkpt or workq
+#PBS -A hpc_masonlab03     #Allocation name
+#PBS -m e                  #Send mail when job ends
+#PBS -M maggie.macpherson@gmail.com #Send mail to this address
+
+module load r/4.0.3/intel-19.0.5
+
+cd $PBS_O_WORKDIR
+
+R --file=/home/maggie/HPC/2b_Migration_Trait_Evolution.R
